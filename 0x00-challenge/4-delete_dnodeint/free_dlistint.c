377a4feffc2c6e613f03c7365e11144e992f4a64
Joy Ada Obekpa <joyobekpa29@gmail.com>
2022-11-13 20:43:54 +0000

    4-delete_dnodeint

--- 0x00-challenge/4-delete_dnodeint/free_dlistint.c
+++ 0x00-challenge/4-delete_dnodeint/free_dlistint.c
@@ -3,18 +3,37 @@
 #include "lists.h"
 
 /**
- * free_dlistint - Free a list
+ * add_dnodeint_end - Add a node at the end of a list
  *
- * @head: A pointer to the first element of the list
+ * @head: The address of the pointer to the first element of the list
+ * @n: The number to store in the new element
+ *
+ * Return: A pointer to the new element
  */
-void free_dlistint(dlistint_t *head)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *node;
+	dlistint_t *new;
+	dlistint_t *l;
 
-	while (head)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 	{
-		node = head;
-		head = head->next;
-		free(node);
+		return (NULL);
 	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	l = *head;
+	while (l->next != NULL)
+	{
+		l = l->next;
+	}
+	l->next = new;
+	new->prev = l;
+	return (new);
 }
